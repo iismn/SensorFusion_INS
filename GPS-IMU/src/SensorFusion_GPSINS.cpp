314cc3897a7f8cb7a743f29de63aae729264c1f5
iismn <iismn@kaist.ac.kr>
2021-08-17 22:24:29 +0900

    PARTIAL_RELEASE

--- GPS-IMU/src/SensorFusion_GPSINS.cpp
+++ GPS-IMU/src/SensorFusion_GPSINS.cpp
@@ -18,27 +18,19 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-
+#include <gtsam_unstable/nonlinear/IncrementalFixedLagSmoother.h>
 using namespace gtsam;
 using namespace std;
 
+using gtsam::symbol_shorthand::X; // Pose       (x,y,z,r,p,y)
+using gtsam::symbol_shorthand::V; // Velocity   (xdot,ydot,zdot)
+using gtsam::symbol_shorthand::B; // Bias       (ax,ay,az,gx,gy,gz)
 
-struct Position{
-  double X;
-  double Y;
-  double Z;
-  double Roll;
-  double Pitch;
-  double Yaw;
-} EIGEN_ALIGN16;
-
-typedef Position ROS_Pose6D;
-
-class LocalMAP_OSM_Localization : public ParamServer
+class SensorFusion_GPSINS : public ParamServer
 {
 
 public:
-
+    std::mutex mtx;
     // GTSAM OPTIMIZER
     NonlinearFactorGraph gtSAMgraph;
     Values initialEstimate;
@@ -47,7 +39,9 @@ public:
     Values isamCurrentEstimate;
     Values LM_CurrentEstimate;
 
-    LevenbergMarquardtParams LM_params;
+    gtsam::ISAM2 optimizer;
+    gtsam::NonlinearFactorGraph graphFactors;
+    gtsam::Values graphValues;
 
     gtsam::PreintegratedImuMeasurements *imuIntegratorOpt_;
     gtsam::PreintegratedImuMeasurements *imuIntegratorImu_;
@@ -59,6 +53,19 @@ public:
     gtsam::noiseModel::Diagonal::shared_ptr correctionNoise_Reset;
     gtsam::Vector noiseModelBetweenBias;
 
+    gtsam::Pose3 gpsPose;
+    gtsam::Pose3 prevPose_;
+    gtsam::Vector3 prevVel_;
+    gtsam::NavState prevState_;
+    gtsam::imuBias::ConstantBias prevBias_;
+
+    gtsam::NavState ODOM_Prv;
+    gtsam::imuBias::ConstantBias BIAS_Prv;
+
+    gtsam::Pose3 gps2IMU = gtsam::Pose3(gtsam::Rot3(1, 0, 0, 0), gtsam::Point3(-extTrans.x(), -extTrans.y(), -extTrans.z()));
+    gtsam::Pose3 IMU2gps = gtsam::Pose3(gtsam::Rot3(1, 0, 0, 0), gtsam::Point3(extTrans.x(), extTrans.y(), extTrans.z()));
+
+
 
     // DATA CONATINER
     std::deque<nav_msgs::Odometry> gpsQueue;
@@ -67,49 +74,40 @@ public:
     std::deque<sensor_msgs::Imu> imuQueImu;
 
     // Pre-INTG PARAM
+    bool systemInitialized = false;
     bool doneFirstOpt = false;
     double lastImuT_imu = -1;
     double lastImuT_opt = -1;
-
+    double utm_x_init = 0;
+    double utm_y_init = 0;
+    double utm_z_init = 0;
     const double delta_t = 0;
+    int key = 1;
 
-    Eigen::MatrixXd poseCovariance;
-    Eigen::Vector3f START_POINT;
-    Eigen::Vector3f END_POINT;
-    Eigen::Vector3f DISTANCE_POINT;
+    double UPDATED_qX = 0;
+    double UPDATED_qY = 0;
+    double UPDATED_qZ = 0;
+    double UPDATED_qW = 0;
 
-    ROS_Pose6D TEMP_POSE;
-
-    nav_msgs::Odometry INS_Odometry;
-
-    ros::Publisher pubCloudRegisteredRaw;
+    // ROS
+    nav_msgs::Odometry  INS_Odometry;
+    nav_msgs::Path      INS_Path;
+    nav_msgs::Path      GPS_Path;
+    ros::Subscriber subIMU;
     ros::Subscriber subGPS;
-
-    map<int, pair< ROS_Pose6D ,pcl::PointCloud<PointTypeRGB>>> DATA_CONTAINER;
+    ros::Publisher  pubINS;
+    ros::Publisher  pubPath;
+    ros::Publisher  pubGTPath;
 
     // --------------------------------------------------------------------------------------------------------------------------------------------
-    SensorFusion_GPSINS()
-    {
-        ISAM2Params ISAM_params;
-        ISAM_params.relinearizeThreshold = 0.1;
-        ISAM_params.relinearizeSkip = 1;
-        isam = new ISAM2(ISAM_params);
+    SensorFusion_GPSINS(){
 
-        LM_params.orderingType = Ordering::METIS;
+        subIMU =  nh.subscribe<sensor_msgs::Imu>("imu/data", 2000, &SensorFusion_GPSINS::imuHandler, this, ros::TransportHints().tcpNoDelay());
+        subGPS =  nh.subscribe<sensor_msgs::NavSatFix> ("ublox/fix", 200, &SensorFusion_GPSINS::gpsHandler, this, ros::TransportHints().tcpNoDelay());
 
-        subIMU = nh.subscribe<vgicp_sam::cloud_info>("imu/data", 1, &mapOptimization::imuHandler, this, ros::TransportHints().tcpNoDelay());
-        subGPS = nh.subscribe<nav_msgs::Odometry> ("gps/fix", 200, &mapOptimization::gpsHandler, this, ros::TransportHints().tcpNoDelay());
-
-        pubINS = nh.advertise<nav_msgs::Odometry>("ins/odom", 1);
-
-        allocateMemory();
-    }
-
-    void allocateMemory()
-    {
-        imuIntegratorImu_ = new gtsam::PreintegratedImuMeasurements(p, prior_imu_bias); // setting up the IMU integration for IMU message thread
-        imuIntegratorOpt_ = new gtsam::PreintegratedImuMeasurements(p, prior_imu_bias); // setting up the IMU integration for optimization
-
+        pubINS =  nh.advertise<nav_msgs::Odometry>("ins/odom", 1);
+        pubPath = nh.advertise<nav_msgs::Path>("ins/fused_path", 1);
+        pubGTPath = nh.advertise<nav_msgs::Path>("ins/gps_path", 1);
         // VGICP-SAM : SHARED PTR BOOST
         boost::shared_ptr<gtsam::PreintegrationParams> p = gtsam::PreintegrationParams::MakeSharedU(imuGravity);
         p->accelerometerCovariance  = gtsam::Matrix33::Identity(3,3) * pow(imuAccNoise, 2); // acc white noise in continuous
@@ -124,9 +122,9 @@ public:
         correctionNoise_Reset = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(6) << 1, 1, 1, 1, 1, 1).finished()); // rad,rad,rad,m, m, m
         noiseModelBetweenBias = (gtsam::Vector(6) << imuAccBiasN, imuAccBiasN, imuAccBiasN, imuGyrBiasN, imuGyrBiasN, imuGyrBiasN).finished();
 
-        PCL_DATA.reset(new pcl::PointCloud<PointTypeRGB>());
-    };
-
+        imuIntegratorImu_ = new gtsam::PreintegratedImuMeasurements(p, prior_imu_bias); // setting up the IMU integration for IMU message thread
+        imuIntegratorOpt_ = new gtsam::PreintegratedImuMeasurements(p, prior_imu_bias); // setting up the IMU integration for optimization
+    }
     // --------------------------------------------------------------------------------------------------------------------------------------------
     void imuHandler(const sensor_msgs::Imu::ConstPtr& IMU_RAW){
 
@@ -134,9 +132,13 @@ public:
 
         // A-1. Transformate IMU Coordinate (ENU)
         sensor_msgs::Imu IMU_CURRENT = imuConverter(*IMU_RAW);
-
         imuQueOpt.push_back(IMU_CURRENT);
         imuQueImu.push_back(IMU_CURRENT);
+
+        UPDATED_qX = IMU_CURRENT.orientation.x;
+        UPDATED_qY = IMU_CURRENT.orientation.y;
+        UPDATED_qZ = IMU_CURRENT.orientation.z;
+        UPDATED_qW = IMU_CURRENT.orientation.w;
 
         // RETURN (IMU not INITIALIZED)
         if (doneFirstOpt == false)
@@ -152,13 +154,13 @@ public:
 
         // A-2. GTSAM Predict IMU State (IMU Preintegration)
         gtsam::NavState ODOM_Now = imuIntegratorImu_->predict(ODOM_Prv, BIAS_Prv);
-        gtsam::Pose3 IMU_Pose3 = gtsam::Pose3(ODOM_Now.quaternion(), ODOM_Now.position());
-        gtsam::Pose3 GPS_Pose3 = IMU_Pose3.compose(imu2Lidar);
 
+        gtsam::Pose3 IMU_Pose3 = gtsam::Pose3(ODOM_Now.quaternion(), ODOM_Now.position());
+        gtsam::Pose3 GPS_Pose3 = IMU_Pose3.compose(gps2IMU);
         // A-4. ROS Publish GTSAM NAV STATE
         INS_Odometry.header.stamp = IMU_CURRENT.header.stamp;
         INS_Odometry.header.frame_id = odometryFrame;
-        INS_Odometry.child_frame_id = "navsat_link";
+        INS_Odometry.child_frame_id = "odom_imu";
 
         INS_Odometry.pose.pose.position.x = GPS_Pose3.translation().x();
         INS_Odometry.pose.pose.position.y = GPS_Pose3.translation().y();
@@ -174,12 +176,31 @@ public:
         INS_Odometry.twist.twist.angular.x = IMU_CURRENT.angular_velocity.x + BIAS_Prv.gyroscope().x();
         INS_Odometry.twist.twist.angular.y = IMU_CURRENT.angular_velocity.y + BIAS_Prv.gyroscope().y();
         INS_Odometry.twist.twist.angular.z = IMU_CURRENT.angular_velocity.z + BIAS_Prv.gyroscope().z();
-        pubImuOdometry.publish(INS_Odometry);
+
+        pubINS.publish(INS_Odometry);
+
+
+        INS_Path.header.stamp = IMU_CURRENT.header.stamp;
+        INS_Path.header.frame_id = odometryFrame;
+
+        geometry_msgs::PoseStamped pose_stamped;
+        pose_stamped.header.stamp = IMU_CURRENT.header.stamp;
+        pose_stamped.header.frame_id = odometryFrame;
+        pose_stamped.pose.position.x = GPS_Pose3.translation().x();
+        pose_stamped.pose.position.y = GPS_Pose3.translation().y();
+        pose_stamped.pose.position.z = GPS_Pose3.translation().z();
+        pose_stamped.pose.orientation.x = GPS_Pose3.rotation().toQuaternion().x();
+        pose_stamped.pose.orientation.y = GPS_Pose3.rotation().toQuaternion().y();
+        pose_stamped.pose.orientation.z = GPS_Pose3.rotation().toQuaternion().z();
+        pose_stamped.pose.orientation.w = GPS_Pose3.rotation().toQuaternion().w();
+
+        INS_Path.poses.push_back(pose_stamped);
+
+        pubPath.publish(INS_Path);
 
     }
-
     // --------------------------------------------------------------------------------------------------------------------------------------------
-    void gpsHandler(const sensor_msgs::NavSatFix:ConstPtr& GPS_RAW){
+    void gpsHandler(const sensor_msgs::NavSatFix::ConstPtr& GPS_RAW){
 
         std::lock_guard<std::mutex> lock(mtx);
         double currentCorrectionTime = ROS_TIME(GPS_RAW);
@@ -191,16 +212,18 @@ public:
         double utm_x = 0;
         double utm_y = 0;
         std::string utm_zone_;
-        NavsatConversions::LLtoUTM(GPS_RAW->latitude, GPS_RAW->longitude, utm_y, utm_x, utm_zone_);
+        GeonavTransform::NavsatConversions::LLtoUTM(GPS_RAW->latitude, GPS_RAW->longitude, utm_y, utm_x, utm_zone_);
                                                                       //  NORTH  EAST
         // B-1. COMPILE LiDAR Odom
         float p_x = utm_x;
         float p_y = utm_y;
         float p_z = GPS_RAW->altitude;
-        float r_x = INS_Odometry.pose.pose.orientation.x;
-        float r_y = INS_Odometry.pose.pose.orientation.y;
-        float r_z = INS_Odometry.pose.pose.orientation.z;
-        float r_w = INS_Odometry.pose.pose.orientation.w;
+        float r_x = UPDATED_qX;
+        float r_y = UPDATED_qY;
+        float r_z = UPDATED_qZ;
+        float r_w = UPDATED_qW;
+
+
 
         // gtsam::Vector Vector3(3);
         // Vector3 << max(GPS_RAW->position_covariance[0], 1.0f), max(GPS_RAW->position_covariance[4], 1.0f), max(GPS_RAW->position_covariance[8], 1.0f);
@@ -208,15 +231,17 @@ public:
         // gtsam::GPSFactor gps_factor(cloudKeyPoses3D->size(), gtsam::Point3(p_x, p_y, p_z), gps_noise);
         // gtSAMgraph.add(gps_factor);
 
-        bool degenerate = (int)INS_Odometry->pose.covariance[0] == 1 ? true : false;
-        gtsam::Pose3 gpsPose = gtsam::Pose3(gtsam::Rot3::Quaternion(r_w, r_x, r_y, r_z), gtsam::Point3(p_x, p_y, p_z));
-
-
         // B-!. Optimize Fail -> RESET GTSAM Optimizer
         if (systemInitialized == false)
         {
             resetOptimization();
 
+            utm_x_init = p_x;
+            utm_y_init = p_y;
+            utm_z_init = p_z;
+            p_x = p_x - utm_x_init;
+            p_y = p_y - utm_y_init;
+            p_z = p_z - utm_z_init;
             // pop old IMU message
             while (!imuQueOpt.empty())
             {
@@ -229,7 +254,8 @@ public:
                     break;
             }
             // initial pose
-            prevPose_ = gpsPose.compose(gps2Imu);
+            gtsam::Pose3 gpsPose_INIT = gtsam::Pose3(gtsam::Rot3::Quaternion(r_w, r_x, r_y, r_z), gtsam::Point3(p_x, p_y, p_z));
+            prevPose_ = gpsPose_INIT.compose(gps2IMU);
             gtsam::PriorFactor<gtsam::Pose3> priorPose(X(0), prevPose_, priorPoseNoise);
             graphFactors.add(priorPose);
             // initial velocity
@@ -254,8 +280,20 @@ public:
 
             key = 1;
             systemInitialized = true;
+
             return;
         }
+        else{
+            p_x = p_x - utm_x_init;
+            p_y = p_y - utm_y_init;
+            p_z = p_z - utm_z_init;
+            cout << "POSE3 : " << p_x << " " << p_y << " " << p_z << endl;
+            gpsPose = gtsam::Pose3(gtsam::Rot3::Quaternion(r_w, r_x, r_y, r_z), gtsam::Point3(p_x, p_y, p_z));
+        }
+
+
+        // bool degenerate = (int)INS_Odometry->pose.covariance[0] == 1 ? true : false;
+        // gtsam::Pose3 gpsPose = gtsam::Pose3(gtsam::Rot3::Quaternion(r_w, r_x, r_y, r_z), gtsam::Point3(p_x, p_y, p_z));
 
 
         // B-2. RESET Graph : Optimize Performance
@@ -319,13 +357,15 @@ public:
 
         // B-3T. ADD GPS Factor to NonlinearFactorGraph
         gtsam::Vector Vector3(3);
-        Vector3 << max(GPS_RAW->position_covariance[0], 1.0f), max(GPS_RAW->position_covariance[4], 1.0f), max(GPS_RAW->position_covariance[8], 1.0f);
-        correctionNoise_Nominal = noiseModel::Diagonal::Variances(Vector3);
+        Vector3 << (float)GPS_RAW->position_covariance[0], (float)GPS_RAW->position_covariance[4], (float)GPS_RAW->position_covariance[8];
+        noiseModel::Diagonal::shared_ptr correctionNoise_GPS = noiseModel::Diagonal::Variances(Vector3);
 
-        gtsam::Pose3 curPose = gpsPose.compose(gps);
-        gtsam::PriorFactor<gtsam::Pose3> pose_factor(X(key), curPose, degenerate ? correctionNoise_Reset : correctionNoise_Nominal);
-        graphFactors.add(pose_factor);//!----------------------------------------------------------------------------------------------------------
-
+        correctionNoise_GPS->print("GPS Noise:\n");
+        gtsam::Pose3 curPose = gpsPose.compose(gps2IMU);
+        // gtsam::PriorFactor<gtsam::Pose3> pose_factor(X(key), curPose, correctionNoise_Nominal);
+        // graphFactors.add(pose_factor);
+        gtsam::GPSFactor gps_factor(X(key), gtsam::Point3(p_x, p_y, p_z), correctionNoise_GPS);
+        graphFactors.add(gps_factor);
 
         // B-3T. ADD IMU Predicted Value
         gtsam::NavState propState_ = imuIntegratorOpt_->predict(prevState_, prevBias_);
@@ -358,8 +398,8 @@ public:
 
 
         // 2. after optiization, re-propagate imu odometry preintegration
-        prevStateOdom = prevState_;
-        prevBiasOdom  = prevBias_;
+        ODOM_Prv = prevState_;
+        BIAS_Prv  = prevBias_;
         // first pop imu message older than current correction data
         double lastImuQT = -1;
         while (!imuQueImu.empty() && ROS_TIME(&imuQueImu.front()) < currentCorrectionTime - delta_t)
@@ -371,7 +411,7 @@ public:
         if (!imuQueImu.empty())
         {
             // reset bias use the newly optimized bias
-            imuIntegratorImu_->resetIntegrationAndSetBias(prevBiasOdom);
+            imuIntegratorImu_->resetIntegrationAndSetBias(BIAS_Prv);
             // integrate imu message from the beginning of this optimization
             for (int i = 0; i < (int)imuQueImu.size(); ++i)
             {
@@ -389,18 +429,64 @@ public:
         ++key;
         doneFirstOpt = true;
 
+
+
+        GPS_Path.header.stamp = GPS_RAW->header.stamp;
+        GPS_Path.header.frame_id = odometryFrame;
+
+        geometry_msgs::PoseStamped pose_stamped;
+        pose_stamped.header.stamp = GPS_RAW->header.stamp;
+        pose_stamped.header.frame_id = odometryFrame;
+        pose_stamped.pose.position.x = p_x;
+        pose_stamped.pose.position.y = p_y;
+        pose_stamped.pose.position.z = p_z;
+        pose_stamped.pose.orientation.x = r_x;
+        pose_stamped.pose.orientation.y = r_y;
+        pose_stamped.pose.orientation.z = r_z;
+        pose_stamped.pose.orientation.w = r_w;
+
+        GPS_Path.poses.push_back(pose_stamped);
+
+        pubGTPath.publish(GPS_Path);
+
     }
-
     // --------------------------------------------------------------------------------------------------------------------------------------------
-    void publish_Odom(){
+    bool failureDetection(const gtsam::Vector3& velCur, const gtsam::imuBias::ConstantBias& biasCur){
+        Eigen::Vector3f vel(velCur.x(), velCur.y(), velCur.z());
+        if (vel.norm() > 30)
+        {
+            ROS_WARN("Large velocity, reset IMU-preintegration!");
+            return true;
+        }
 
+        Eigen::Vector3f ba(biasCur.accelerometer().x(), biasCur.accelerometer().y(), biasCur.accelerometer().z());
+        Eigen::Vector3f bg(biasCur.gyroscope().x(), biasCur.gyroscope().y(), biasCur.gyroscope().z());
+        if (ba.norm() > 1.0 || bg.norm() > 1.0)
+        {
+            ROS_WARN("Large bias, reset IMU-preintegration!");
+            return true;
+        }
+
+        return false;
     }
-
     // --------------------------------------------------------------------------------------------------------------------------------------------
-    gtsam::Pose3 trans2gtsamPose(Position SE3_IN)
-    {
-        return gtsam::Pose3(gtsam::Rot3::RzRyRx(SE3_IN.Yaw, SE3_IN.Pitch, SE3_IN.Roll),
-                                  gtsam::Point3(SE3_IN.X, SE3_IN.Y, SE3_IN.Z));
+    void resetOptimization(){
+        gtsam::ISAM2Params optParameters;
+        optParameters.relinearizeThreshold = 0.1;
+        optParameters.relinearizeSkip = 1;
+        optimizer = gtsam::ISAM2(optParameters);
+
+        gtsam::NonlinearFactorGraph newGraphFactors;
+        graphFactors = newGraphFactors;
+
+        gtsam::Values NewGraphValues;
+        graphValues = NewGraphValues;
+    }
+    // --------------------------------------------------------------------------------------------------------------------------------------------
+    void resetParams(){
+        lastImuT_imu = -1;
+        doneFirstOpt = false;
+        systemInitialized = false;
     }
 
 };
@@ -412,9 +498,9 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "SensorFusion_GNSS");
 
     SensorFusion_GPSINS SF;
-    ROS_INFO("\033[1;32m----> Local OSM Localization Start \033[0m");
+    ROS_INFO("\033[1;32m----> GPS-INS FUSION START \033[0m");
 
-    ros::MultiThreadedSpinner spinner(4);
+    ros::MultiThreadedSpinner spinner(6);
     spinner.spin();
 
     return 0;
